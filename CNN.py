6274e25b1c6aceb79135ce447a13a14996f33733
AMAN MIGLANI <miglaniaman84@gmail.com>
2020-05-25 21:08:36 +0530

    1st

--- CNN.py
+++ CNN.py
@@ -92,12 +92,12 @@ train_datagen = ImageDataGenerator(
         horizontal_flip=True)
 test_datagen = ImageDataGenerator(rescale=1./255)
 training_set   = train_datagen.flow_from_directory(
-        '/training_set/',
+        '/mlops/cnn_dataset/training_set/',
         target_size=(64, 64 ),
         batch_size=32,
         class_mode='binary')
 test_set = test_datagen.flow_from_directory(
-        '/test_set/',
+        '/mlops/cnn_dataset/test_set/',
         target_size=(64, 64),
         batch_size=32,
         class_mode='binary')
